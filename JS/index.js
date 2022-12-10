4d24f7bdb0bea18f11d16727d3935381f3c6e7b0
Martin Moreno <martin.moreno.j04@gmail.com>
2022-12-10 14:14:41 -0500

    definición de clases en js

---
+++ JS/index.js
@@ -0,0 +1,3 @@
+var car = new Car("ASE908",new Account("Erick","467231"))
+car.passenger = 4
+car.printDataCar()
---
+++ Python/main.py
@@ -0,0 +1,5 @@
+from account import Account
+
+cuenta = Account("Jose","234")
+print(vars(cuenta))
+

60836be6eb3a519b84e41ba11860b8a5037fc585
Martin Moreno <martin.moreno.j04@gmail.com>
2022-12-11 15:04:38 -0500

    Se hace herencia en javascript

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
-var car = new Car("ASE908",new Account("Erick","467231"))
+var car = new UberX("ASE376",new Driver("Antonio Mario","33466"),"Nissan","2020")
 car.passenger = 4
-car.printDataCar()
+car.printDataCar();
+
+var car2 = new UberPool("LOI109", new Driver("Pablo Ortega","45521"),"Tesla","ps2")
+car2.passenger = 2
+car2.printDataCar();
